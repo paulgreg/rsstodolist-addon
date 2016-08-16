ac2c2af31ea7c161dfb5d8596a72ac993b03cd30
Grégory Paul <paulgreg@gmail.com>
2016-08-16 22:46:16 +0200

    Web Extension migration

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-Description 
+Description
 ===========
 
 rsstodolist is a service to build RSS feed, hosted on Google App Engine, at https://rsstodolist.appspot.com.
 You can deploy the server code on your own server (https://github.com/paulgreg/rsstodolist-server) to host your private instance.
 
 This is the rsstodolist add-on for Firefox, allowing to add URL to a RSS feed, available on the [Mozilla Add-on repository](https://addons.mozilla.org/fr/firefox/addon/rss-todolist-addon/).
-
-Run and deploy
---------------
-
-First, you need to install the [Firefox SDK Add-on](https://addons.mozilla.org/en-US/developers/docs/sdk/latest/) via a `npm install jpm`.
-To run the addon, type `./node_modules/.bin/jpm run` and `./node_modules/.bin/jpm xpi` to build the package.
 
 Attribution
 --------------
--- index.js
+++ index.js
@@ -1,102 +1,113 @@
-var { ToggleButton } = require("sdk/ui/button/toggle");
-var tabs = require("sdk/tabs");
-var panels = require("sdk/panel");
-var Request = require('sdk/request').Request;
-var data = require("sdk/self").data;
-var storage = require("sdk/simple-storage").storage;
-var notifications = require("sdk/notifications");
+var $add           = document.querySelector('#add');
+var $del           = document.querySelector('#del');
+var $goto          = document.querySelector('#goto');
+var $feed          = document.querySelector('#feed');
+var $title         = document.querySelector("#title");
+var $desc          = document.querySelector("#description");
+var $more          = document.querySelector('#more');
+var $less          = document.querySelector('legend');
+var $detail        = document.querySelector('fieldset');
+var $customServer  = document.querySelector('#customServer');
+var $customUrl     = document.querySelector('input[type=url]');
+var more           = false;
 
-var appspotServerUrl = "https://rsstodolist.appspot.com/";
-var server = appspotServerUrl;
+function getServer () {
+    return $customServer.checked ? $customUrl.value : "https://rsstodolist.appspot.com/";
+}
 
-var initialHeight = 50;
-var expandedHeight= 180;
+function openMore () {
+    $more.style.display = 'none';
+    $detail.style.display = 'block';
+    more = true;
+    save();
+}
 
-var updateStorage = function(message) {
-    server = (message.customServer) ? message.customUrl : appspotServerUrl;
-    if (storage.feed !== message.feed) {
-        storage.feed = message.feed;
-    }
-    if (storage.customServer !== message.customServer) {
-        storage.customServer = message.customServer;
-    }
-    if (storage.customUrl !== message.customUrl) {
-        storage.customUrl = message.customUrl;
-    }
-};
+$more.addEventListener('click', openMore, false);
 
-var panel = panels.Panel({
-    contentURL: data.url("rsstodolist.html"),
-    contentScriptFile: [data.url("interactions.js")],
-    width: 380,
-    height: initialHeight,
-    onMessage: function(message) {
-        switch(message.action) {
-            case 'collapse':
-                this.height = initialHeight;
-                break;
-            case 'expand':
-                this.height = expandedHeight;
-                break;
-            case 'goto':
-                updateStorage(message);
-                tabs.open(server +"?name="+ message.feed);
-                panel.hide();
-                break;
-            case 'add':
-            case 'del':
-                updateStorage(message);
-                sendRequest(message);
-                panel.hide();
-                break;
+function save () {
+    chrome.storage.local.set({
+        'prefs': {
+            'feed': $feed.value,
+            'customUrl': $customUrl.value,
+            'customServer': $customServer.checked,
+            'more': more
         }
-    },
-    onHide: function() {
-        button.state('window', {checked: false});
-    }
-});
-
-var button = ToggleButton({
-    id: "rsstodolist",
-    label: "RSS Todo List",
-    icon: "./icon.png",
-    onChange: function(state) {
-        if (state.checked) {
-            initializeUI();
-            panel.show({
-                position: button
-            });
-        }
-    }
-});
-
-var initializeUI = function() {
-    this.panel.postMessage({
-        'feed': storage.feed,
-        'title': tabs.activeTab.title,
-        'description': '',
-        'customServer': storage.customServer || false,
-        'customUrl': storage.customUrl
     });
-};
+}
+function load (data) {
+    $feed.value = data.prefs.feed || "somename";
+    $customUrl.value = data.prefs.customUrl || "https://";
+    $customServer.checked = data.prefs.customServer;
+    (data.prefs.more) && openMore();
+}
 
-var sendRequest = function(message) {
-    var url = server + message.action +"?name="+ message.feed +"&title="+ encodeURIComponent(message.title || "")  +"&description="+ encodeURIComponent(message.description || "") +"&url="+ encodeURIComponent(tabs.activeTab.url);
-    Request({
-        url: url,
-        onComplete: function(response){
-            if(response.status == 200) {
-                notifications.notify({
-                    text: "Feed '"+ server + storage.feed +"' updated",
-                    iconURL: data.url("icon.png")
-                });
-            } else {
-                console.error("Sending request ERROR : " + response.status);
-                notifications.notify({
-                    text: "A problem occured while updating feed '"+ server + storage.feed +"'",
-                    iconURL: data.url("icon.png")
-                });
-            }
-        }
-    }).get();
-};
+$goto.addEventListener('click', () => {
+    browser.tabs.create({
+        url: getServer() + '?name=' + encodeURIComponent($feed.value)
+    });
+    save();
+}, false);
+
+function notify(status, msg) {
+    browser.notifications.create("rsstodolist-notification", {
+        type: "basic",
+        iconUrl: status ? "ok.png" : "error.png",
+        title: "rsstodolist " + (status ? "ok" : "error"),
+        message: msg
+    });
+}
+
+$add.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        var url = [
+            getServer(),
+            "add",
+            "?name=",
+            encodeURIComponent($feed.value) ,
+            "&title=",
+            encodeURIComponent($title.value || "") ,
+            "&description="+ encodeURIComponent($desc.value || "") ,
+            "&url=",
+            encodeURIComponent(tabs[0].url)
+        ].join("");
+        var req = new XMLHttpRequest();
+        req.open('GET', url, false);
+        req.send(null);
+        notify(req.status === 200, url);
+        save();
+    });
+}, false);
+
+$del.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        var url = [
+            getServer(),
+            "del",
+            "?name=",
+            encodeURIComponent($feed.value) ,
+            "&url=",
+            encodeURIComponent(tabs[0].url)
+        ].join("");
+        var req = new XMLHttpRequest();
+        req.open('GET', url, false);
+        req.send(null);
+        notify(req.status === 200, url);
+        save();
+    });
+}, false);
+
+
+$less.addEventListener('click', () => {
+    $more.style.display = 'block';
+    $detail.style.display = 'none';
+    more = false;
+    save();
+}, false);
+
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.local.get('prefs', load);
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        $title.value = tabs[0].title || "";
+    });
+});
+
