651be1ab8587b63fbe45a668a297114074202922
Grégory Paul <me@gpaul.me>
2023-02-24 18:35:03 +0100

    Fix context menu

--- background-script.js
+++ background-script.js
@@ -3,26 +3,20 @@ import { load, send } from "./common.js";
 const MENU_ADD = 'rsstodolist-add'
 const MENU_DEL = 'rsstodolist-del'
 
+chrome.contextMenus.create({
+    id: MENU_ADD,
+    title: `Add link`,
+    contexts: ["link"]
+})
 
-browser.runtime.onInstalled.addListener(() => {
+chrome.contextMenus.create({
+    id: MENU_DEL,
+    title: `Remove link`,
+    contexts: ["link"]
+})
 
-    chrome.contextMenus.create({
-        id: MENU_ADD,
-        title: `Add link`,
-        contexts: ["link"]
-    })
-
-    chrome.contextMenus.create({
-        id: MENU_DEL,
-        title: `Remove link`,
-        contexts: ["link"]
-    })
-
-    chrome.contextMenus.onClicked.addListener(info => {
-        load(prefs => {
-            const link = info.linkText.startsWith("https://") || info.linkText.startsWith("http://") ? info.linkText : info.linkUrl
-            console.log(link)
-            send(prefs.server, info.menuItemId === MENU_ADD, prefs.feed, link)
-        })
+chrome.contextMenus.onClicked.addListener(info => {
+    load(prefs => {
+        send(prefs.server, info.menuItemId === MENU_ADD, prefs.feed, info.linkUrl)
     })
 })
