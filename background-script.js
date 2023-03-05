f1d1326a0c0d67de2945aea8c609a903415fa387
Grégory Paul <me@gpaul.me>
2023-03-05 12:45:18 +0100

    Add feed name in context menu

--- background-script.js
+++ background-script.js
@@ -3,22 +3,28 @@ import { load, send } from "./common.js";
 const MENU_ADD = 'rsstodolist-add'
 const MENU_DEL = 'rsstodolist-del'
 
-chrome.contextMenus.removeAll(() => { 
-    chrome.contextMenus.create({
-        id: MENU_ADD,
-        title: `Add link`,
-        contexts: ["link"]
-    })
+const installContextMenu = (prefs) =>
+    chrome.contextMenus.removeAll(() => { 
+        chrome.contextMenus.create({
+            id: MENU_ADD,
+            title: `Add link to feed « ${prefs.feed} »`,
+            contexts: ["link"]
+        })
 
-    chrome.contextMenus.create({
-        id: MENU_DEL,
-        title: `Remove link`,
-        contexts: ["link"]
-    })
+        chrome.contextMenus.create({
+            id: MENU_DEL,
+            title: `Remove link from feed « ${prefs.feed} »`,
+            contexts: ["link"]
+        })
 
-    chrome.contextMenus.onClicked.addListener(info => {
-        load(prefs => {
+        chrome.contextMenus.onClicked.addListener(info => {
             send(prefs.server, info.menuItemId === MENU_ADD, prefs.feed, info.linkUrl)
         })
     })
+
+load(prefs => installContextMenu(prefs))
+
+chrome.storage.onChanged.addListener((o) => {
+    const prefs = o?.prefs?.newValue
+    if (prefs) installContextMenu(prefs)
 })
